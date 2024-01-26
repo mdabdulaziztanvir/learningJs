5c1a48f1f5024cdae90d356072159790bb68256a
mdabdulaziztanvir <abdulazizztanvir@gmail.com>
2024-01-26 10:39:39 +0600

    Javascript function

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-const minNumber = 1;
-const maxNumber = 100;
-
-const randomNumber =
-  Math.floor(Math.random() * maxNumber - minNumber + 1) + minNumber;
-
-let attempts = 0;
-let guess;
-
-let running = true;
-
-while (running) {
-  guess = window.prompt(`Enter a number between ${minNumber} to ${maxNumber}`);
-
-  guess = Number(guess);
-
-  if (isNaN(guess) || guess > maxNumber || guess < minNumber) {
-    window.alert(`please enter a valid Number`);
-  } else {
-    attempts++;
-
-    guess < randomNumber
-      ? window.alert(`${guess} number is low. Try again...`)
-      : guess > randomNumber
-      ? window.alert(`${guess} number is High. Try again...`)
-      : (window.alert(
-          `Successfull... The right number is ${randomNumber}. You have finished it with ${attempts} attempts. Refresh to play again.`
-        ),
-        (running = false));
-  }
+function something(username, age) {
+  //theese are parameters
+  console.log("happy birthday to you");
+  console.log(`happy birthday to ${username}`);
+  console.log(`you are ${age} years old`);
 }
+something("tanvir", 24); //inside values are arguments
+something("spongebob", 34); //inside values are arguments
+something("someOne", 3); //inside values are arguments
+
+function add(a, b) {
+  return a + b;
+}
+
+let ans = add(2, 3);
+console.log(ans);
+
+function isEven(number) {
+  (number % 2 === 0) ?  console.log("Even Number") : console.log("Odd number");
+}
+
+isEven(24)
+
+function validEmail(email) {
+  return email.includes("@") ? true : false;
+}
+
+console.log(validEmail("leonmask.com"));
